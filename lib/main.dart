d02fcc87bfbc5165372f141e8a6595f747df9d4e
nvrr <sureshvarma809@gmail.com>
2020-10-13 12:04:22 +0530

    2.Testing with Flutter and dart: mock test

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,15 @@ void main() {
   runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('News')
+        ),
+      )
+    );
+  }
+}
