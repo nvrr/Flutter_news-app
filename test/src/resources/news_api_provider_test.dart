acf272847790a3e20c971012bd6c45b239317a14
nvrr <sureshvarma809@gmail.com>
2020-10-13 13:05:26 +0530

    2.Testing with Flutter and dart: testing fetchTopIds and fetchItem, are they return id's, item correctly by making fake API and using MockCleint in test library of HTTP library

--- test/src/resources/news_api_provider_test.dart
+++ test/src/resources/news_api_provider_test.dart
@@ -5,11 +5,35 @@ import 'package:http/http.dart';
 import 'package:http/testing.dart';
 
 void main() {
-  test('FetchTopIds returns a list of ids',() {
+  test('FetchTopIds returns a list of ids',() async{
     // setup of test case
-    final sum = 1 + 3;
+    final newsApi = NewsApiProvider();
+    newsApi.client = MockClient((request) async{
+      return Response(json.encode([1,2,3,4]), 200);
+    });
+
+    final ids = await newsApi.fetchTopIds();
 
     //expectation
-    expect(sum, 4);
+    expect(ids, [1,2,3,4]);
+    
+  });
+
+  test('FetchItem returns a item model',() async{
+    // setup of test case
+    final newsApi = NewsApiProvider();
+    newsApi.client = MockClient((request) async{
+      final jsonMap = {'id': 123};
+      return Response(json.encode(jsonMap), 200);
+    });
+
+    final item = await newsApi.fetchItem(999);//rather than 123 lets justdo like 999, i wouldn't want to pass in 123 here 
+                                          // Bcz u will notice that iam passing in our response object has an idea of 123 and 
+                                          // right now our test is not actually trying to make sure that like we're fetching the correct ID ,
+                                          //we definitly could modify the test do that ,but i would not want to put 123 right here 
+                                          //bcz that wouldn't make it seem like that's we were trying to make an assertion for that.
+
+    //expectation
+    expect(item.id, 123);
   });
 }
