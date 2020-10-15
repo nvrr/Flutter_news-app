68c94ec3095c5d5683f843c7d99f4c86fa14b9b8
nvrr <sureshvarma809@gmail.com>
2020-10-15 15:03:26 +0530

    3. Offline Data Storage: More On The Repository

--- lib/src/resources/repository.dart
+++ lib/src/resources/repository.dart
@@ -7,8 +7,20 @@ class Repository {
   NewsApiProvider apiProvider = NewsApiProvider();
   NewsDbProvider dbProvider = NewsDbProvider();
 
-  fetchTopIds() {}
+  fetchTopIds() {
+    return apiProvider.fetchTopIds();
+  }
 
-  fetchItem() {}
-  
+  fetchItem(int id) async{
+    var item = await dbProvider.fetchItem(id);
+    if (item != null) {
+      return item;
+    }
+
+    item = await apiProvider.fetchItem(id);
+    dbProvider.addItemToDb(item);
+
+    return item;
+  }
+
 }
