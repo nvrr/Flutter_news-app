11479ff2de9177c705d8850ee93679ba7b989e15
nvrr <sureshvarma809@gmail.com>
2020-10-17 23:23:25 +0530

    5. On Demand Widget Rendering: The Stories Provider: here we code the provider/inheritedWidget

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
 class NewsList extends StatelessWidget {
   @override
@@ -8,33 +7,7 @@ class NewsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Top News')
       ),
-      body: buildList()
-    );
-  }
-
-  Widget buildList() {
-    return ListView.builder(
-      itemCount: 1000,
-      itemBuilder: (context, index) {
-        return FutureBuilder(
-          future: getFuture(),
-          builder: (context, snapshot) {
-            return Container(
-              height: 80.0,
-              child: snapshot.hasData
-                    ? Text('holla visible $index')
-                    : Text('i havent fetched data yet $index'),
-            );
-          }
-        );
-      }
-    );
-  }
-
-  getFuture() {
-    return Future.delayed(
-      Duration(seconds: 2),
-      () => 'hi',
+      body: Text('News top News')
     );
   }
 
