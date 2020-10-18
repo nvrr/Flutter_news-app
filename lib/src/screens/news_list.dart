f6b9e8ff410cd9742ccd939d41acd7f32193638e
nvrr <sureshvarma809@gmail.com>
2020-10-18 12:06:09 +0530

    5. On Demand Widget Rendering: Exposing Bloc Getters -here we have ability to get info from repository and add it into StramContr

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import '../blocs/stories_provider.dart';
 
 class NewsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = StoriesProvider.of(context);
+    
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News')
