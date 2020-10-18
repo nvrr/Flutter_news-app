8ad7e9ede92ee868666776b9fa350117ed4e333a
nvrr <sureshvarma809@gmail.com>
2020-10-18 16:28:33 +0530

    5. On Demand Widget Rendering: Showing CircularProgressIndicator while Loading

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -34,7 +34,7 @@ class NewsList extends StatelessWidget {
       stream: bloc.topIds,
       builder: (context, AsyncSnapshot<List<int>> snapshot) {
         if (!snapshot.hasData) {
-          return Text('Still waiting on ids');
+          return Center(child: CircularProgressIndicator());
         }
 
         return ListView.builder(
