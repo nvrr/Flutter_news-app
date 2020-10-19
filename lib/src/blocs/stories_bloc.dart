56c1270609301b26ad5d72579375ff0c5ee6058e
nvrr <sureshvarma809@gmail.com>
2020-10-19 23:07:07 +0530

    5. On Demand Widget Rendering: Single Tranformer Application - to make transform single and not make it everytime we use constructor

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -8,26 +8,34 @@ class StoriesBloc {
   final _topIds = PublishSubject<List<int>>();
   final _items = BehaviorSubject<int>();
 
+  Stream <Map<int, Future<ItemModel>>> items;
+
   // Getters to Streams
   Stream<List<int>> get topIds => _topIds.stream;
 
-  // -------------------------------------- bad-----
-
-  // Dont do this, its not good, it invokes _itemsTransformer for every widget individually
-   // the problm is that every separate widget that tries to acess this getter, will run
-   // this code "_items.stream.transform(_itemsTransformer())" and 
-   //every separate one is going to gets own individual Cache object ,this is 
-   // this is not shared with any other widgets,which is without a doubt not ideal
-   // =>> all it means is that we need to make sure that 
-   // we only try to apply this transformer "_itemsTransformer()" right here exactly
-   // exactly one time to our stream
-   get items => _items.stream.transform(_itemsTransformer());
-  // here we need to make sure we only apply the tranform one time
-
-  // -------------------------------------------- bad-----
-
   // Getters to Sinks
   Function(int) get fetchItem => _items.sink.add;
+
+  StoriesBloc() {
+    // Gotcha -when we call transform on a stream it does not modify the original stream.
+    // instead it returns new stream , so new stream is return from this 
+    // "_items.stream.transform(_itemsTransformer());" line of code and that is the 
+    // stream that we wanted to expose to the outside world
+    // so we need to somehow take the stream that is produced by this line of 
+    // code "_items.stream.transform(_itemsTransformer());" and make sure that everyone in outside
+    // world can get access to it.  So in order to do so i'm going to add a new instance variable
+    // up above at the top " items" --"Stream <Map<int, Future<ItemModel>>> items;"
+    // so this is the instance variable thats going to hold a reference to the 
+    // transform streamed stream that we just put together right below , so intialize items = "below code"
+
+    // OK so now any time we create a new stories block
+    // we immediately create a new behavior subjects we declare instance variable of items but we don't assign
+    // of a value to its constructor function is invoked.
+    // We take the items stream controller we take the stream off we apply a transform to it exactly one time
+    // and then we return that transform streambed and assign it to this item's variable.
+    // And so now everyone in the outside world all the outside widgets that exist can all listen to this item's Stream right here.
+    items = _items.stream.transform(_itemsTransformer());
+  }
 
   fetchTopIds() async{
     final ids = await _repository.fetchTopIds();
