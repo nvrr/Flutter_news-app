374d5eba4589ceb6f8c7881c41a8e2a11af10d80
nvrr <sureshvarma809@gmail.com>
2020-10-20 23:50:27 +0530

    5. On Demand Widget Rendering: A Laborious Refactor

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -6,35 +6,18 @@ import '../resources/repository.dart';
 class StoriesBloc {
   final _repository = Repository();
   final _topIds = PublishSubject<List<int>>();
-  final _items = BehaviorSubject<int>();
-
-  Stream <Map<int, Future<ItemModel>>> items;
+  final _itemsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
+  final _itemsFetcher = PublishSubject<int>();
 
   // Getters to Streams
   Stream<List<int>> get topIds => _topIds.stream;
+  Stream<Map<int, Future<ItemModel>>> get items => _itemsOutput.stream;
 
   // Getters to Sinks
-  Function(int) get fetchItem => _items.sink.add;
+  Function(int) get fetchItem => _itemsFetcher.sink.add;
 
   StoriesBloc() {
-    // Gotcha -when we call transform on a stream it does not modify the original stream.
-    // instead it returns new stream , so new stream is return from this 
-    // "_items.stream.transform(_itemsTransformer());" line of code and that is the 
-    // stream that we wanted to expose to the outside world
-    // so we need to somehow take the stream that is produced by this line of 
-    // code "_items.stream.transform(_itemsTransformer());" and make sure that everyone in outside
-    // world can get access to it.  So in order to do so i'm going to add a new instance variable
-    // up above at the top " items" --"Stream <Map<int, Future<ItemModel>>> items;"
-    // so this is the instance variable thats going to hold a reference to the 
-    // transform streamed stream that we just put together right below , so intialize items = "below code"
-
-    // OK so now any time we create a new stories block
-    // we immediately create a new behavior subjects we declare instance variable of items but we don't assign
-    // of a value to its constructor function is invoked.
-    // We take the items stream controller we take the stream off we apply a transform to it exactly one time
-    // and then we return that transform streambed and assign it to this item's variable.
-    // And so now everyone in the outside world all the outside widgets that exist can all listen to this item's Stream right here.
-    items = _items.stream.transform(_itemsTransformer());
+     _itemsFetcher.stream.transform(_itemsTransformer()).pipe(_itemsOutput);
   }
 
   fetchTopIds() async{
@@ -55,6 +38,7 @@ class StoriesBloc {
 
   dispose() {
     _topIds.close();
-    _items.close();
+    _itemsFetcher.close();
+    _itemsOutput.close();
   }
 }
