29de7199902a2ce72a234ae235368a431ef72450
nvrr <sureshvarma809@gmail.com>
2020-10-24 19:23:37 +0530

    Navigation with Flutter: Testing Recursive Fetching

--- lib/src/blocs/comments_bloc.dart
+++ lib/src/blocs/comments_bloc.dart
@@ -22,6 +22,7 @@ class CommentsBloc {
   _commentsTransformer() {
     return ScanStreamTransformer<int, Map<int, Future<ItemModel>>>(
       (cache, int id, _index) {
+        print(_index);
         cache[id] = _repository.fetchItem(id);
         cache[id].then((ItemModel item) {
           item.kids.forEach((kidId) => fetchItemWithComments(kidId));
