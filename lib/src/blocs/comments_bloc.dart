ecff59f7cc0840ab5e68646845e88516253d87d9
nvrr <sureshvarma809@gmail.com>
2020-10-24 13:19:59 +0530

    Navigation with Flutter: More On Comments Bloc

--- lib/src/blocs/comments_bloc.dart
+++ lib/src/blocs/comments_bloc.dart
@@ -1,1 +1,21 @@
-class CommentsBloc {}
+import 'dart:async';
+import 'package:rxdart/rxdart.dart';
+import '../models/item_model.dart';
+import '../resources/repository.dart';
+
+class CommentsBloc {
+  final _commentsFetcher = PublishSubject<int>();
+  final _commentsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
+
+  // Sreams
+  Stream<Map<int, Future<ItemModel>>> get itemsWithComments =>
+      _commentsOutput.stream;
+
+  // Sink
+  Function(int) get fetchItemWithComments => _commentsFetcher.sink.add;
+
+  dispose() {
+    _commentsFetcher.close();
+    _commentsOutput.close();
+  }
+}
