54dba69445d7330b17cf72da8c3a4336102d44c5
nvrr <sureshvarma809@gmail.com>
2020-10-24 18:48:43 +0530

    Navigation with Flutter: Connecting The Comments Provider

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'screens/news_list.dart';
 import 'blocs/stories_provider.dart';
-import 'screens/news_detail.dart'; 
+import 'screens/news_detail.dart';
+import 'blocs/comments_provider.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StoriesProvider(
-      child: MaterialApp(
-        title: 'News!',
-        onGenerateRoute: routes,
+    return CommentsProvider(
+          child: StoriesProvider(
+        child: MaterialApp(
+          title: 'News!',
+          onGenerateRoute: routes,
+        ),
       ),
     );
   }
@@ -24,7 +27,10 @@ class App extends StatelessWidget {
         } else {
           return MaterialPageRoute(
             builder: (context) {
+              final commentsBloc = CommentsProvider.of(context);
               final itemId = int.parse(settings.name.replaceFirst('/', ''));
+
+              commentsBloc.fetchItemWithComments(itemId);
 
               return NewsDetail(
                 itemId: itemId,
