9526cad775dcbe7430a19b6ec6b77fde5a125f4f
nvrr <sureshvarma809@gmail.com>
2020-10-25 15:03:01 +0530

    Navigation with Flutter: Text Styling

--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -38,10 +38,24 @@ class NewsDetail extends StatelessWidget {
               return Text('LoadinG');
             }
 
-            return Text(itemSnapshot.data.title);
+            return buildTitle(itemSnapshot.data);
           }
         );
       }
+    );
+  }
+
+  Widget buildTitle(ItemModel item) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: Text(
+        item.title,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold
+        )
+      )
     );
   }
 }
