324ac1fcfbda8dfd94eadf53d9e04dfe442403ee
nvrr <sureshvarma809@gmail.com>
2020-10-26 06:36:18 +0530

    Navigation with Flutter: Showing Individual Comments

--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import '../blocs/comments_provider.dart';
 import '../models/item_model.dart';
+import '../widgets/comment.dart';
 
 class NewsDetail extends StatelessWidget {
   final int itemId;
@@ -46,10 +47,16 @@ class NewsDetail extends StatelessWidget {
   }
 
   Widget buildList(ItemModel item, Map<int, Future<ItemModel>> itemMap) {
+    final children = <Widget>[];
+    children.add(buildTitle(item));
+
+    final commentList = item.kids.map((kidId) {
+      return Comment(itemId: kidId, itemMap: itemMap);
+    }).toList();
+    children.addAll(commentList);
+
     return ListView(
-      children: <Widget>[
-        buildTitle(item),
-      ],
+      children: children,
     );
   }
 
