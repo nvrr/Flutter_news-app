742e14d4fed61c87d0ba51716ab6bb4dfa704d5b
nvrr <sureshvarma809@gmail.com>
2020-10-26 11:50:56 +0530

    Navigation with Flutter: Handling Deleted Comments

--- lib/src/widgets/comment.dart
+++ lib/src/widgets/comment.dart
@@ -22,7 +22,7 @@ class Comment extends StatelessWidget {
         final children = <Widget>[
           ListTile(
             title: Text(item.text),
-            subtitle: Text(item.by)
+            subtitle: item.by == '' ? Text('Deleted') :Text(item.by),
           ),
           Divider(),
         ];
